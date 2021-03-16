c5564fde9912e0c0f74f9de4aa85c3181b5a8d50
a.isakov <a.isakov@skbkontur.ru>
2021-03-16 17:36:10 +0500

    new(ui): cool ui

---
+++ src/App/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Скоро здесь будет классный интерфейс классного сервиса!
+        </div>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
